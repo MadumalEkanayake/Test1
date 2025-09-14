aa5167f2234a4966ec3bf034eb400b7c1a2af036
MadumalEkanayake <madumal9musou@gmail.com>
2025-09-14 20:19:09 +0530

    4comm

--- src/Prc/Arr.java
+++ src/Prc/Arr.java
@@ -2,7 +2,7 @@ package Prc;
 
 public class Arr {
     public static void main(String[] args) {
-        int[] a = {1, 6, 4, 23, 2,3};
+        int[] a = {1, 6, 4, 23, 2,3,10};
 
         for (int i = a.length - 1; i >= 0; i--) {
             System.out.print(a[i] + " ");
