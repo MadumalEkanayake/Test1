2e8b27a6be75f7af6a697e39e7c1074840797b55
MadumalEkanayake <madumal9musou@gmail.com>
2025-09-14 19:56:18 +0530

    change commit

--- src/Prc/Arr.java
+++ src/Prc/Arr.java
@@ -2,10 +2,10 @@ package Prc;
 
 public class Arr {
     public static void main(String[] args) {
-        int[] a = {1, 6, 4, 23, 2};
+        int[] a = {1, 6, 4, 23, 2,3};
 
         for (int i = a.length - 1; i >= 0; i--) {
             System.out.print(a[i] + " ");
-        }
+        }//comm
     }
 }
